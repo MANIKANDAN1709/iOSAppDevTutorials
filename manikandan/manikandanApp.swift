b0fb4228acfeb366246184d8fe93c8fdc9c709ed
Manikandan <manikandan17901@gmail.com>
2024-04-03 21:35:31 +0530

    Initial Commit

---
+++ manikandan/manikandanApp.swift
@@ -0,0 +1,17 @@
+//
+//  manikandanApp.swift
+//  manikandan
+//
+//  Created by MANIKANDAN SAMINATHAN on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct manikandanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
